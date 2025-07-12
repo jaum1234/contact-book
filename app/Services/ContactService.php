f273ed6b17c87e45111389ba2da6f2f793025caf
joao coura <joaovitorsouzacoura@gmai.com>
2025-07-12 10:58:56 -0300

    feat: add logging to the service class and check if the address already exists before fetching it from ViaCep

--- app/Services/ContactService.php
+++ app/Services/ContactService.php
@@ -1,32 +1,43 @@
 <?php
 
+namespace App\Services;
+
 use App\Models\Address;
 use App\Models\Contact;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class ContactService {
   public function create(array $data) {
-    $addressInfo = Http::get(config('services.viacep.base_url') . $data['postal_code'] . '/json/')
-        ->throw()
+    if (!Address::where('postal_code', $data['postal_code'])->first()) {
+      Log::info('Fetching address information from ViaCEP', ['postal_code' => $data['postal_code']]);
+  
+      $addressInfo = Http::get(config('services.viacep.base_url') . $data['postal_code'] . '/json/')
+        ->throw()  
         ->json();
-
-    $address = new Address();
-
-    $address->postal_code = $addressInfo['cep'];
-    $address->street = $addressInfo['logradouro'];
-    $address->complement = $addressInfo['complemento'];
-    $address->neighborhood = $addressInfo['bairro'];
-    $address->city = $addressInfo['localidade'];
-    $address->unit = $addressInfo['unidade'];
-    $address->country = 'Brasil';
-    $address->state_abbreviation = $addressInfo['uf'];
-    $address->state = $addressInfo['estado'];
-    $address->ibge_code = $addressInfo['ibge'];
-    $address->gia_code = $addressInfo['gia'];
-    $address->region = $addressInfo['regiao'];
-    $address->area_code = $addressInfo['ddd'];
-    $address->siafi_code = $addressInfo['siafi'];
-    $address->save();
+  
+      Log::info('Address information retrieved successfully', ['address' => $addressInfo]);
+  
+      $address = new Address();
+  
+      $address->postal_code = str_replace('-', '', $addressInfo['cep']);
+      $address->street = $addressInfo['logradouro'];
+      $address->complement = $addressInfo['complemento'];
+      $address->neighborhood = $addressInfo['bairro'];
+      $address->city = $addressInfo['localidade'];
+      $address->unit = $addressInfo['unidade'];
+      $address->country = 'Brasil';
+      $address->state_abbreviation = $addressInfo['uf'];
+      $address->state = $addressInfo['estado'];
+      $address->ibge_code = $addressInfo['ibge'];
+      $address->gia_code = $addressInfo['gia'];
+      $address->region = $addressInfo['regiao'];
+      $address->area_code = $addressInfo['ddd'];
+      $address->siafi_code = $addressInfo['siafi'];
+      $address->save();
+  
+      Log::info('Address saved successfully', ['postal_code' => $address->postal_code]);
+    }
 
     $contact = new Contact();
     $contact->name = $data['name'];
@@ -34,5 +45,7 @@ class ContactService {
     $contact->phone_number = $data['phone_number'];
     $contact->postal_code = $data['postal_code'];
     $contact->save();
+
+    Log::info('Contact saved successfully', ['email' => $contact->email]);
   }
 }
